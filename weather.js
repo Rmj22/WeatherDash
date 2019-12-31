89c11a04f94b2d27fe5b52c2b5a99192543e8331
Robin Jones <RobinJones335@gmail.com>
2019-12-31 13:54:13 -0500

    rewrote the api call after looking at previous examples

---
+++ weather.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var ApiKey = "60cd8f1a2d119cd70b98f1936f0a3ded"
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?"+"q=city&appid=" + ApiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);
+
+        // Converts the temp to Kelvin with the below formula
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + response.main.temp);
+      });
+})
